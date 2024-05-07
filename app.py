05c5290395be4ad24568c024d0fd4dec96818d8f
AshwaqAshu <117159821+AshwaqAshu@users.noreply.github.com>
2024-05-07 19:08:53 +0000

    updated code

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 
-app= Flask(__name__) 
+app = Flask(__name__)
 
-@app.route('/') 
+data = [{'title': 'hello'}]
+
+
+@app.route('/')
 def home():
-  return "Hello World" 
+  return render_template('index.html')
 
-if __name__=='__main__' :
+
+@app.route('/api/json')
+def json():
+  return jsonify(data)
+
+
+if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
